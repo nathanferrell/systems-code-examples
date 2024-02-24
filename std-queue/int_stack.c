a56c190da7cd344cd2cb9c5c745385a4796ad02d
nathan ferrell <nferrell6@gmail.com>
2024-02-24 19:39:19 +0000

    int stack.c and intstack.h edited also Cmake in stdqueue changed

--- std-queue/int_stack.c
+++ std-queue/int_stack.c
@@ -1,7 +1,4 @@
-// int_stack.c
-
 #include "int_stack.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -67,6 +64,78 @@ int int_stack_swap(int_stack_t *stk) {
     int_stack_pop(stk, &next_to_top_value);
     int_stack_push(stk, top_value);
     return int_stack_push(stk, next_to_top_value); // success only if last operation succeeds
+}
+
+/* New stack operations */
+int int_stack_over(int_stack_t *stk) {
+    if (stk->size < 2)
+        return 0;
+    int top_value, next_to_top_value;
+    int_stack_top(stk, &top_value);
+    int_stack_pop(stk, &next_to_top_value);
+    int_stack_push(stk, next_to_top_value);
+    return int_stack_push(stk, top_value);
+}
+
+int int_stack_rot(int_stack_t *stk) {
+    if (stk->size < 3)
+        return 0;
+    int top_value, second_value, third_value;
+    int_stack_pop(stk, &top_value);
+    int_stack_pop(stk, &second_value);
+    int_stack_pop(stk, &third_value);
+    int_stack_push(stk, second_value);
+    int_stack_push(stk, top_value);
+    return int_stack_push(stk, third_value);
+}
+
+int int_stack_drop(int_stack_t *stk) {
+    if (stk->size < 1)
+        return 0;
+    int top_value;
+    return int_stack_pop(stk, &top_value);
+}
+
+int int_stack_2swap(int_stack_t *stk) {
+    if (stk->size < 4)
+        return 0;
+    int d1, d2, d3, d4;
+    int_stack_pop(stk, &d1);
+    int_stack_pop(stk, &d2);
+    int_stack_pop(stk, &d3);
+    int_stack_pop(stk, &d4);
+    int_stack_push(stk, d2);
+    int_stack_push(stk, d1);
+    int_stack_push(stk, d4);
+    return int_stack_push(stk, d3);
+}
+
+int int_stack_2dup(int_stack_t *stk) {
+    if (stk->size < 2)
+        return 0;
+    int d1, d2;
+    int_stack_top(stk, &d1);
+    int_stack_pop(stk, &d2);
+    int_stack_push(stk, d2);
+    return int_stack_push(stk, d1);
+}
+
+int int_stack_2over(int_stack_t *stk) {
+    if (stk->size < 4)
+        return 0;
+    int d1, d2;
+    int_stack_top(stk, &d1);
+    int_stack_pop(stk, &d2);
+    int_stack_push(stk, d2);
+    return int_stack_push(stk, d1);
+}
+
+int int_stack_2drop(int_stack_t *stk) {
+    if (stk->size < 2)
+        return 0;
+    int d1, d2;
+    int_stack_pop(stk, &d1);
+    return int_stack_pop(stk, &d2);
 }
 
 /* Example of how to create a binary operator that works o top two elements (if present) */
--- std-queue/int_stack.h
+++ std-queue/int_stack.h
@@ -1,5 +1,3 @@
-// int_stack.h - interface to the int_stack
-
 #ifndef INT_STACK_H
 #define INT_STACK_H
 
@@ -23,6 +21,13 @@ extern int int_stack_pop(int_stack_t *stk, int *top_value);
 extern int int_stack_top(int_stack_t *stk, int *top_value);
 extern int int_stack_dup(int_stack_t *stk);
 extern int int_stack_swap(int_stack_t *stk);
+extern int int_stack_over(int_stack_t *stk);
+extern int int_stack_rot(int_stack_t *stk);
+extern int int_stack_drop(int_stack_t *stk);
+extern int int_stack_2swap(int_stack_t *stk);
+extern int int_stack_2dup(int_stack_t *stk);
+extern int int_stack_2over(int_stack_t *stk);
+extern int int_stack_2drop(int_stack_t *stk);
 extern int int_stack_add(int_stack_t *stk);
 extern void int_stack_print(int_stack_t *stk, FILE *file);
 extern int int_stack_size(int_stack_t* stk);
